abf9c73b9ede9236d04bb70d43c2dbc93550f1d6
COnokala <C.Onokala@gmail.com>
2019-10-17 16:21:10 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import { Route, Link } from "react-router-dom";
 import Fads from './Fads'
 import Decade from './Decade'
 
+const HomeHeader = styled.header`
+display:flex;
+flex-direction:column;
+height:46px;
+width:100%;
+top:0;
+position:fixed;
+color:white;
+font-size:18px;`
+
+
 function App() {
   return (
-    <div>
-      <h1>Nostaldja</h1>
+    <div className="main">
+      <HomeHeader>
+      <h1 className="nostal">Nostaldja</h1>
       <Link to="/fads">
-        <h3>Fads</h3>
+        <h3 className="fads">Fads</h3>
       </Link>
       <Link to="/decades">
-        <h3>Decades</h3>
+        <h3 className="decades">Decades</h3>
       </Link>
+      </HomeHeader>
       <Route path="/fads" exact component = {Fads} />
       <Route path="/decades" exact component = {Decade} />
     </div>
--- src/Fads.js
+++ src/Fads.js
@@ -1,6 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { Route, Link } from "react-router-dom";
+import './Fads.css'
+import Img from 'react-image'
+
+const myComponent = () => (
+  <Img
+    src={['https://upload.wikimedia.org/wikipedia/commons/d/dc/Ipod-touch-1st-gen.jpg', 
+    'https://sixpl.com/wp-content/uploads/2017/09/Blockchain-and-Cryptocurrency-Content-Writer.jpg',
+    'https://www.dhresource.com/0x0s/f2-albu-g5-M01-79-07-rBVaJFiuqDSAF3I7AAKBk1FyKy0267.jpg/hand-spinner-fidget-spinner-tri-spinner-diy.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Silly_Bandz_2009.jpg/2560px-Silly_Bandz_2009.jpg',
+    'https://us.hellomagazine.com/imagenes/travel/2018012645793/tom-myspace-founder-travel-photographer/0-230-700/myspace-tom-now-t.jpg',
+    'https://img.buzzfeed.com/buzzfeed-static/static/2015-06/23/15/enhanced/webdr08/enhanced-22226-1435087265-4.jpg?downsize=715:*&output-format=auto&output-quality=auto'
+  ]}
+  />
+)
 
 function Fads() {
     const [data, setData] = useState( [] );
